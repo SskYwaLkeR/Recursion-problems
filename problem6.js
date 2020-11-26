96ab2c85ccdbf11598e725b7cb3d10fad9b7ac9a
sskywalker <hkrai39@gmail.com>
2020-11-26 19:36:32 +0530

    small changes

--- problem6.js
+++ problem6.js
@@ -1,1 +1,16 @@
-// find number in sorted array
+
+//find number in an array
+
+function findNumArr(list, itm){
+  
+    let length = list.length - 1; 
+    
+    if(length == -1) return 'no item '
+    if(list[length] == itm) return 'item found'
+    let newArr = list.slice(0,length)
+    // list.pop()
+    return findNumArr(list, itm)
+    
+  }
+
+  findNumArr([1,2,3, 7], 2)
